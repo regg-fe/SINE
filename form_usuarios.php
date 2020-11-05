76bc86d5b5589ba1232fb5d6cd52acf8d76d8185
Overdusk <71523821+Overdusk@users.noreply.github.com>
2020-11-06 00:12:31 +0200

    inserción de usuarios

---
+++ form_usuarios.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Nuevo Usuario</title>
+</head>
+
+<body>
+
+Por favor, rellena los campos descritos a continuacion
+
+<center>
+<form action="add_users.php" method="post">
+
+	Nombre de usuario <input type="text" name="nombre"> <br>
+	Contraseña <input type="password" name="contra"> <br>
+	Validar Contraseña <input type="password" name="contravali"> <br>
+	<input type="submit" value="Enviar">
+	
+</form>
+
+</center>
+
+
+
+</body>
+</html>
